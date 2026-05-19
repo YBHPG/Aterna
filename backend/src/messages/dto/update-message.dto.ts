ec224c306fe6d68bcb9eb46578790ef544deb9a4
Vladislav <70971642+YBHPG@users.noreply.github.com>
2026-05-20 01:13:01 +0300

    fix message-dto

--- backend/src/messages/dto/update-message.dto.ts
+++ backend/src/messages/dto/update-message.dto.ts
@@ -1,7 +1,12 @@
-import { IsString, MaxLength } from "class-validator";
+import { IsOptional, IsString, MaxLength } from "class-validator";
 
 export class UpdateMessageDto {
+    @IsOptional()
     @IsString({ message: "Текст сообщения должен быть строкой" })
     @MaxLength(10000, { message: "Максимальная длина сообщения — 10 000 символов" })
-    content: string;
+    content?: string;
+
+    @IsOptional()
+    @IsString()
+    recipientEmail?: string;
 }
